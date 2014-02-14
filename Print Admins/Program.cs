8ca1297460bc6c9e7faa9685a3238dccf4108a17
philanderadriaan <philander90@msn.com>
2014-02-14 15:16:33 -0800

    Added ability to change permissions.

--- Print Admins/Program.cs
+++ Print Admins/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,12 @@ namespace Print_Admins
     {
         static void Main(string[] args)
         {
+            ProcessStartInfo info = new ProcessStartInfo();
+            info.UseShellExecute = false;
+            info.FileName = "SetACL.exe";
+            info.Arguments = "-on \"\\\\dofp\\DO 309 LJ 1320n (Rosson)\" -ot prn -actn ace -ace \"n:NKSD PrintAdmins;p:full\"";
+            Process.Start(info);
+            Console.ReadLine();
         }
     }
 }
