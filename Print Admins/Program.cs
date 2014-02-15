5afed83620234d756cda3c406572637f66f02ec7
philanderadriaan <philander90@msn.com>
2014-02-14 16:16:40 -0800

    Working on path arguments.

--- Print Admins/Program.cs
+++ Print Admins/Program.cs
@@ -11,11 +11,16 @@ namespace Print_Admins
     {
         static void Main(string[] args)
         {
-            ProcessStartInfo info = new ProcessStartInfo();
-            info.UseShellExecute = false;
-            info.FileName = "SetACL.exe";
-            info.Arguments = "-on \"\\\\dofp\\DO 309 LJ 1320n (Rosson)\" -ot prn -actn ace -ace \"n:NKSD PrintAdmins;p:full\"";
-            Process.Start(info);
+            foreach (String printer_name in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.UseShellExecute = false;
+                info.FileName = "SetACL.exe";
+                info.Arguments = "-on \""+printer_name+"\" -ot prn -actn list";
+                //info.Arguments = "-on \"\\\\dofp\\DO 309 LJ 1320n (Rosson)\" -ot prn -actn ace -ace \"n:NKSD PrintAdmins;p:full\"";
+                Process.Start(info);
+                //Console.WriteLine(printer_name);
+            }
             Console.ReadLine();
         }
     }
