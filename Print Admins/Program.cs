0512b179bf4769ca78009645fd899bf78cc24f23
philanderadriaan <philander90@msn.com>
2014-02-20 14:33:17 -0800

    Replaced repo.

---
+++ Print Admins/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_Admins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            foreach (String printer_name in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.UseShellExecute = false;
+                info.FileName = "SetACL.exe";
+                info.Arguments = "-on \"" + printer_name + "\" -ot prn -actn ace -ace \"n:NKSD PrintAdmins;p:full\"";
+                Process.Start(info);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
