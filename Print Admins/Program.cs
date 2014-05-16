fc2fa1a65954bec173c44ba4879be5d5fd5d4f87
philanderadriaan <philander90@msn.com>
2014-05-16 09:19:55 -0700

    Added imports for System.Drawing.Printing.

--- Print Admins/Program.cs
+++ Print Admins/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Drawing.Printing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ namespace Print_Admins
     {
         static void Main(string[] args)
         {
-            foreach (String printer_name in System.Drawing.Printing.PrinterSettings.InstalledPrinters)
+            foreach (string printer_name in PrinterSettings.InstalledPrinters)
             {
                 ProcessStartInfo info = new ProcessStartInfo();
                 info.UseShellExecute = false;
