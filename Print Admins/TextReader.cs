f460b75c4ba55ad08215e19585ca8b15f7ca7afa
philanderadriaan <philander90@msn.com>
2014-05-16 11:29:16 -0700

    Added text reader.

---
+++ Print Admins/TextReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Print_Admins
+{
+    class TextReader
+    {
+        string path;
+
+        public TextReader(string path)
+        {
+            this.path = path;
+        }
+    }
+}
